20505c024575026c0d233e30ecf6860989ffe762
enoqueJonas <enoque.macanda@gmail.com>
2023-01-16 20:31:22 +0200

    UPDATE Search styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Search.css';
 
 const Search = () => (
         <div className="search">
-            <input type='text' className="searchfield" />
+            <input type='text' className="searchfield" placeholder='Search by name..' />
         </div>
 );
 
