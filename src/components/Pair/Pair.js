ae1362864897b5aa2456615a3e076c09b0af4312
enoqueJonas <enoque.macanda@gmail.com>
2023-01-17 17:08:50 +0200

    ADD img to the Pair component

--- src/components/Pair/Pair.js
+++ src/components/Pair/Pair.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Pair.css';
+import dollar from '../../assets/dollar.png';
+import euro from '../../assets/euro-currency-symbol.png';
 
 const Pair = () => (
-        <div className="pair">
-            <h2>EUR/USD</h2>
-            <p>EUR/USD</p>
-            <p>1.5666</p>
+    <div className="pair">
+        <div className='pair-symbol'>
+            <img src={euro} className='symbol' width='50px' height='50px'/>
+            <span className='separator'>vs</span>
+            <img src={dollar} className='symbol'width='50px' height='50px'/>
         </div>
+        <div className='pair-info-wrap'>
+            <p className='pair-info'>EUR/USD</p>
+            <p className='pair-info'>1.5666</p>
+        </div>
+    </div>
 );
 
 export default Pair;
