376b686f9381f8f5d34d4562733a75f6eda23b43
enoqueJonas <enoque.macanda@gmail.com>
2023-01-17 16:53:19 +0200

    UPDATE PairList styling

--- src/components/PairList/PairList.js
+++ src/components/PairList/PairList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Pair from '../Pair/Pair';
+import './PairList.css';
 
 const PairList = () => (
+    <div className='pair-list-wrap'>
+        <p className='category-type'>Pairs by name</p>
         <div className="pair-list">
             <Pair />
-            <Pair/>
-            <Pair/>
-            <Pair/>
+            <Pair />
+            <Pair />
+            <Pair />
+            <Pair />
+            <Pair />
         </div>
+    </div>
 );
 
 export default PairList;
