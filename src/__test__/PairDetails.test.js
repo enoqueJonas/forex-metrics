00d3073c20f9ec4ad46221692a4ff939349f1b35
enoqueJonas <enoque.macanda@gmail.com>
2023-01-18 21:48:40 +0200

    FIX linters

--- src/__test__/PairDetails.test.js
+++ src/__test__/PairDetails.test.js
@@ -1,43 +1,41 @@
-import { pairClicked } from "../redux/pairDetails/pairDetails";
-import { PAIR_CLICKED } from "../redux/pairDetails/pairDetails";
-import pairDetailsReducer from "../redux/pairDetails/pairDetails";
+import pairDetailsReducer, { pairClicked, PAIR_CLICKED } from '../redux/pairDetails/pairDetails';
 
 describe('Pair Details Tests', () => {
-    it('Action created successfuly', () => {
-        const payload = {
-            ticker: 'EUR/USD',
-            bid: 1.6372,
-            ask: 1.9283,
-            high: 1.7821,
-            low: 1.2432
-        }
+  it('Action created successfuly', () => {
+    const payload = {
+      ticker: 'EUR/USD',
+      bid: 1.6372,
+      ask: 1.9283,
+      high: 1.7821,
+      low: 1.2432,
+    };
 
-        const result = pairClicked(payload)
+    const result = pairClicked(payload);
 
-        expect({
-            type: PAIR_CLICKED,
-            payload: {
-                ticker: 'EUR/USD',
-                bid: 1.6372,
-                ask: 1.9283,
-                high: 1.7821,
-                low: 1.2432
-            }
-        }).toEqual(result);
-    })
-    it('Action dispatched succesfully', () => {
-        const payload = {
-            ticker: 'EUR/USD',
-            bid: 1.6372,
-            ask: 1.9283,
-            high: 1.7821,
-            low: 1.2432
-        }
+    expect({
+      type: PAIR_CLICKED,
+      payload: {
+        ticker: 'EUR/USD',
+        bid: 1.6372,
+        ask: 1.9283,
+        high: 1.7821,
+        low: 1.2432,
+      },
+    }).toEqual(result);
+  });
+  it('Action dispatched succesfully', () => {
+    const payload = {
+      ticker: 'EUR/USD',
+      bid: 1.6372,
+      ask: 1.9283,
+      high: 1.7821,
+      low: 1.2432,
+    };
 
-        const action = pairClicked(payload)
+    const action = pairClicked(payload);
 
-        const result = pairDetailsReducer({}, action);
+    const result = pairDetailsReducer({}, action);
 
-        expect(payload).toEqual(result);
-    })
-})
+    expect(payload).toEqual(result);
+  });
+});
